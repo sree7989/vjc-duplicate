6c974d21cd2c7f03277fd53f3b6c373b3736e985
sakthi <sakthijessy26@gmail.com>
2025-08-22 16:27:52 +0530

    added

--- app/work-abroad/[country]/[visas]/Canadapages/Canadaopenwp.jsx
+++ app/work-abroad/[country]/[visas]/Canadapages/Canadaopenwp.jsx
@@ -29,7 +29,7 @@ const Canadapr = () =>{
   <div className="flex-shrink-0 mx-auto lg:mx-0">
     <Image
       src="/canada-work-permit.png"
-      alt="Canada Family Visa"
+      alt="Canada Open Work Visa"
       width={500}
       height={300}
       className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:w-80 lg:h-70 object-contain shadow-lg rounded-lg"
