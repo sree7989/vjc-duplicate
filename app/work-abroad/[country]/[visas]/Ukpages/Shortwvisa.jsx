8473810f124ff826167ada4e6c9027ebb7d88daf
sakthi <sakthijessy26@gmail.com>
2025-08-23 11:17:36 +0530

    added

--- app/work-abroad/[country]/[visas]/Ukpages/Shortwvisa.jsx
+++ app/work-abroad/[country]/[visas]/Ukpages/Shortwvisa.jsx
@@ -39,7 +39,7 @@ const CanadaFS = () => {
         <div className="flex-shrink-0 mx-auto lg:mx-0">
           <Image
             src="/ukshorttermimg.jpg"
-            alt="Image description"
+            alt="Short Term Work Visa"
             width={500}
             height={300}
             className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:w-80 lg:h-70 object-contain shadow-lg rounded-lg"
