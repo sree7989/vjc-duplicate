b25b019cf7665024075aa2a2f6506fb88f5c7f36
sakthi <sakthijessy26@gmail.com>
2025-08-25 13:20:52 +0530

    added

--- app/schengen-visas/[country]/Swedenvisit.jsx
+++ app/schengen-visas/[country]/Swedenvisit.jsx
@@ -23,7 +23,7 @@ const Swedenvisit = () => {
         <div className="flex-shrink-0 mx-auto lg:mx-0">
           <Image
             src="/swedentimg.jpg"
-            alt="Image description"
+            alt="Sweden Tourist Visa Vjc Overseas"
             width={500}
             height={300}
             className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:w-80 lg:h-70 object-contain shadow-lg rounded-lg"
